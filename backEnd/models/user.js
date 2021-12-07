c7b167e168fe42125e45448182e46e8ef98d164e
LeoAvram <leo.avramovic@gmail.com>
2021-12-07 22:45:34 +0100

    39 changing "_id"key to "id" - more frontend friendly

--- backEnd/models/user.js
+++ backEnd/models/user.js
@@ -9,4 +9,12 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+})
+
+userSchema.set('toJSON', {
+    virtuals: true,
+})
+
 exports.User = mongoose.model('User', userSchema);
