54cf7c80a211927e4e0b55d884585502c137bd92
LeoAvram <leo.avramovic@gmail.com>
2021-12-07 14:40:52 +0100

    first commit

---
+++ backEnd/routes/categories.js
@@ -0,0 +1,53 @@
+const {Category} = require('../models/category');
+const express = require('express');
+const router = express.Router();
+
+// get all categories
+router.get(`/`, async (req, res) => {
+    const categoryList = await Category.find();
+
+    if (!categoryList) {
+        res.status(500).json({success: false})
+    }
+    res.send(categoryList);
+})
+
+
+// Create a category
+router.post('/', async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    })
+    category = await category.save();
+
+    if (!category) {
+        return res.status(404).send('the category cannot be created')
+    }
+    res.send(category);
+})
+
+// Delete a category
+router.delete('/:id', (req, res) => {
+    Category.findByIdAndRemove(req.params.id).then(category => {
+        if (category) {
+            return res.status(200).json({
+                success: true,
+                message: "The category is deleted"
+            })
+        } else {
+            return res.status(404).json({
+                success: false,
+                message: "Category not found"
+            })
+        }
+    }).catch(err => {
+        return res.status(400).json({
+            success: false,
+            error: err
+        })
+    })
+})
+
+module.exports = router;
