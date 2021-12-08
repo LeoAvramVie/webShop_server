c0f9a0f3feca3544fcdaa34c5f326d8cc99acd45
LeoAvram <leo.avramovic@gmail.com>
2021-12-08 19:06:27 +0100

    end 58

--- backEnd/routes/orders.js
+++ backEnd/routes/orders.js
@@ -1,14 +1,52 @@
 const {Order} = require('../models/order');
 const express = require('express');
+const {Category} = require("../models/category");
+const {OrderItem} = require("../models/oderItem");
+const {raw} = require("express");
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
+//get all orders / orderList
+router.get(`/`, async (req, res) => {
     const orderList = await Order.find();
 
-    if(!orderList) {
+    if (!orderList) {
         res.status(500).json({success: false})
     }
     res.send(orderList);
 })
 
-module.exports =router;
+//create order
+router.post('/', async (req, res) => {
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async orderitem => {
+        let newOrderItem = new OrderItem({
+            quantity: orderitem.quantity,
+            product: orderitem.product
+        })
+
+        newOrderItem = await newOrderItem.save()
+
+        return newOrderItem._id
+    }))
+    const orderItemsIdsResolved = await orderItemsIds;
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
+    })
+    order = await order.save();
+
+    if (!order) {
+        return res.status(404).send('the order cannot be created')
+    }
+    res.send(order);
+})
+
+module.exports = router;
