19224e364d8ea51e6744ab36b2b6c092d124dee2
LeoAvram <leo.avramovic@gmail.com>
2021-12-16 23:50:39 +0100

    finish Orders capter 6

--- backEnd/routes/orders.js
+++ backEnd/routes/orders.js
@@ -45,6 +45,14 @@ router.post('/', async (req, res) => {
         return newOrderItem._id
     }))
     const orderItemsIdsResolved = await orderItemsIds;
+    //total price of an order (calculation)
+    const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId) => {
+        const oderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+        const totalPrice = (oderItem.product.price * oderItem.quantity);
+        return totalPrice;
+    }));
+
+    const totalPrice = totalPrices.reduce((a,b) => a + b,0)
 
     let order = new Order({
         orderItems: orderItemsIdsResolved,
@@ -55,7 +63,7 @@ router.post('/', async (req, res) => {
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
+        totalPrice: totalPrice,
         user: req.body.user,
     })
     order = await order.save();
@@ -66,6 +74,83 @@ router.post('/', async (req, res) => {
     res.send(order);
 })
 
+//update status of an order
+router.put('/:id', async (req, res)=>{
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status
+        },
+        {new: true}
+    );
+
+    if (!order){
+        return res.status(400).send('the order cannot be updated')
+    }
+    res.send(order)
+})
+
+//delete order
+router.delete('/:id', (req, res) => {
+    //delete the related orderItem from orderItemTable
+    Order.findByIdAndRemove(req.params.id).then(async order => {
+        if (order) {
+            order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            })
+            return res.status(200).json({
+                success: true,
+                message: "The order is deleted"
+            })
+        } else {
+            return res.status(404).json({
+                success: false,
+                message: "order not found"
+            })
+        }
+    }).catch(err => {
+        return res.status(400).json({
+            success: false,
+            error: err
+        })
+    })
+})
+
+//show total sales of the eshop
+router.get("/get/totalsales", async (req, res)=>{
+    const totalSales = await Order.aggregate([
+        {$group: { _id: null, totalsales: {$sum: '$totalPrice'}}}
+    ])
+    if (!totalSales){
+        return res.status(400).send('The order sales cannot be generated')
+    }
+    res.send({totalSales: totalSales.pop().totalsales})
+})
+
+//count orders
+router.get('/get/count', async (req, res) => {
+    const orderCount = await Order.countDocuments();
+
+    if (!orderCount) {
+        res.status(500).json({success: false});
+    }
+    res.send({orderCount: orderCount});
+});
+
+//get List of Order History
+router.get(`/get/userorders/:userid`, async (req, res) => {
+
+    const userOrderList = await Order.find({user: req.params.userid}).populate({
+        path:'orderItems', populate: {
+            path: 'product', populate: 'category'}
+    } ).sort({'dateOrdered': -1});
+
+
+    if (!userOrderList) {
+        res.status(500).json({success: false})
+    }
+    res.send(userOrderList);
+})
 
 
 module.exports = router;
